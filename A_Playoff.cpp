b12b20706d33bdca8406ae1407be0471ca2a1db9
phoenix-aditya <ranout.aditya@gmail.com>
2022-03-10 21:25:11 +0530

    update

---
+++ A_Playoff.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+typedef long long ll;
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define wh(t) ll t;cin>>t;while(t--)
+#define inn freopen("input.txt", "r", stdin)
+#define outt freopen("output.txt", "w", stdout)
+#define f(i,a,b) for(ll i=a;i<b;++i)
+
+// aditya ranaut - phoenix_aditya
+
+
+void solve(){
+    ll n;
+    cin>>n;
+    ll ans = pow(2,n)-1;
+
+    cout<<ans<<endl;
+    
+}
+
+int main(){
+    fast;
+    wh(t)
+        solve();
+    
+    return 0;
+}
---
+++ B_Prove_Him_Wrong.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+typedef long long ll;
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define wh(t) ll t;cin>>t;while(t--)
+#define inn freopen("input.txt", "r", stdin)
+#define outt freopen("output.txt", "w", stdout)
+#define f(i,a,b) for(ll i=a;i<b;++i)
+
+// aditya ranaut - phoenix_aditya
+
+
+void solve(){
+    ll n;
+    cin>>n;
+    
+    if(n>19){
+        cout<<"NO\n";
+        return;
+    }
+    cout<<"YES\n";
+    for(int i=0;i<n;i++){
+        ll temp = pow(3,i);
+        cout<<temp<<" ";
+    }cout<<"\n";
+    
+}
+
+int main(){
+    fast;
+    wh(t)
+        solve();
+    
+    return 0;
+}
