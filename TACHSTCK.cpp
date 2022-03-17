40dc7cf96e95dfceada775a2305efb6331ad7298
phoenix-aditya <ranout.aditya@gmail.com>
2022-03-17 21:31:34 +0530

    update

--- TACHSTCK.cpp
+++ TACHSTCK.cpp
@@ -1,28 +1,31 @@
 using namespace std;
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define ll long long
 int main()
 {
     ll n, d;
-    
-    cin>>n>>d;
-    
+
+    cin >> n >> d;
+
     ll arr[n];
-    
-    for(int i=0;i<n;i++)
+
+    for (int i = 0; i < n; i++)
     {
-        cin>>arr[i];
+        cin >> arr[i];
     }
-    
-    sort(arr,arr+n);
-    ll counter=0;
-    for(int i=0;i<n-1;)
+
+    sort(arr, arr + n);
+    ll counter = 0;
+    for (int i = 0; i < n - 1;)
     {
-        if(abs(arr[i]-arr[i+1])<=d)
-        {counter++;i+=2;}
+        if (abs(arr[i] - arr[i + 1]) <= d)
+        {
+            counter++;
+            i += 2;
+        }
         else
             i++;
     }
-    cout<<counter<<"\n";
+    cout << counter << "\n";
     return 0;
 }
